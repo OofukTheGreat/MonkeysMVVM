4fdc0d29e2313dcc65d9bd2f27fa788e608a1996
User <User@LAB3-3>
2024-02-19 12:53:58 +0200

    idk things

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,9 +1,12 @@
 ﻿namespace MonkeysMVVM;
+using MonkeysMVVM.Views;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+		Routing.RegisterRoute("MonkeyList", typeof(MonkeysPage));
+		Routing.RegisterRoute("MonkeyView", typeof(ShowMonkeyView));
 	}
 }
